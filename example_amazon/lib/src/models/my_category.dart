a38e97c68f15eba16bc799daa819b4d7ccc4f7a3
markkiepe <markkiepe2@gmail.com>
2024-06-14 13:34:12 +0200

    feat(example): add amazon product page example

---
+++ example_amazon/lib/src/models/my_category.dart
@@ -0,0 +1,8 @@
+import "package:flutter_product_page/flutter_product_page.dart";
+
+class MyCategory extends ProductPageShop {
+  const MyCategory({
+    required super.id,
+    required super.name,
+  });
+}
---
+++ example_amazon/lib/src/utils/go_router.dart
@@ -0,0 +1,26 @@
+import "package:flutter/material.dart";
+import "package:go_router/go_router.dart";
+
+CustomTransitionPage buildScreenWithFadeTransition<T>({
+  required BuildContext context,
+  required GoRouterState state,
+  required Widget child,
+}) =>
+    CustomTransitionPage<T>(
+      key: state.pageKey,
+      child: child,
+      transitionsBuilder: (context, animation, secondaryAnimation, child) =>
+          FadeTransition(opacity: animation, child: child),
+    );
+
+CustomTransitionPage buildScreenWithoutTransition<T>({
+  required BuildContext context,
+  required GoRouterState state,
+  required Widget child,
+}) =>
+    CustomTransitionPage<T>(
+      key: state.pageKey,
+      child: child,
+      transitionsBuilder: (context, animation, secondaryAnimation, child) =>
+          child,
+    );
