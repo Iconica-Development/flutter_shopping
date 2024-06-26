bafa7cd0a0671a7971753eafe1653fb7b6a284c9
Freek van de Ven <freek@freekvandeven.nl>
2024-06-26 15:36:28 +0200

    feat: remove example apps

--- packages/flutter_product_page/example/lib/models/my_shop.dart
+++
@@ -1,8 +0,0 @@
-import "package:flutter_product_page/flutter_product_page.dart";
-
-class MyShop extends ProductPageShop {
-  const MyShop({
-    required super.id,
-    required super.name,
-  });
-}
